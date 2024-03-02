e191c1e405e99d11a1ee89a78db80c662aef4d72
Stirbuartem <stirbuartem8@gmail.com>
2024-03-02 11:56:30 +0300

    Initial commit

---
+++ src/main/java/org/example/engine/TelegramBotHandler.java
@@ -0,0 +1,8 @@
+package org.example.engine;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBotHandler {
+
+}
